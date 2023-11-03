46c323e54dc4445c2198192f3d4a4123c0cf4cef
BartoszRajewicz <szejn3@gmail.com>
2023-11-03 23:32:27 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,3 +2,11 @@ const ul = document.querySelector("#categories");
 const listItemsCount = ul.querySelectorAll("li.item").length;
 console.log("Number of categories: " + listItemsCount);
 
+const categoryList = document.querySelectorAll(".item");
+
+categoryList.forEach((item) => {
+  const categoryName = item.querySelector("h2");
+  const elementCount = item.querySelectorAll("ul li").length;
+  console.log("Category: " + categoryName.textContent);
+  console.log("Elements: " + elementCount);
+});
