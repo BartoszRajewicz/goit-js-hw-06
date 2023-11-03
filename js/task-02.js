c4368bc73ca3f4cadb7018837af64dd056f64249
BartoszRajewicz <szejn3@gmail.com>
2023-11-04 00:11:56 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const newList = document.createElement("li");
+  newList.textContent = ingredient;
+  ingredientsList.appendChild(newList);
+});
