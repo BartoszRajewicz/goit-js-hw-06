688026b2a14a661cb0b67ac22a2cf92b3ed525c8
BartoszRajewicz <szejn3@gmail.com>
2023-11-06 22:25:37 +0100

    added js code

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = loginForm;
+
+  if (email.value === "" || password.value === "") {
+    alert("Wypełnij wszystkie pola formularza!");
+  } else {
+    console.log(email.value, password.value);
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
